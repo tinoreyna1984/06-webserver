49b1e16d91c436ed6e204685141490e89e5965b5
Tino Reyna <tinoreyna1984@gmail.com>
2022-07-21 02:38:50 -0500

    actualizo archivos

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 // Servir contenido estático
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home', {titulo: "Principal"});
